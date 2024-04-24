aa49ff9120126ac55ccca225734a5f616f7c682e
phac123 <55650387+phac123@users.noreply.github.com>
2024-04-24 20:30:04 +0800

    Update run.sh

--- SuperGLUE/run.sh
+++ SuperGLUE/run.sh
@@ -1,1 +1,1 @@
-python cli.py --relation_id=WSC --num_class=2 --batch_size=32 --lr=3e-5 --cuda_device=cuda:0 --evaluate_type=Accuracy --a_lr=5e-5 --p_lr=5e-5 --a_epochs=25 --p_epochs=35 --gan_epochs=3
+python cli.py --relation_id=WSC --num_class=2 --batch_size=32 --lr=3e-5 --cuda_device=cuda:0 --evaluate_type=Accuracy --a_lr=5e-5 --p_lr=5e-5 --a_epochs=35 --p_epochs=35 --gan_epochs=2
