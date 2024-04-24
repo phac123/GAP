56bd90354df0883dbff9568e92171b256b2a0a63
phac123 <55650387+phac123@users.noreply.github.com>
2024-04-24 20:32:34 +0800

    Update run.sh

--- GLUE/run.sh
+++ GLUE/run.sh
@@ -1,9 +1,3 @@
-python cli.py --relation_id=CoLA --num_class=2 --batch_size=32 --lr=5e-5 --a_lr=5e-5 --p_lr=4e-3 --cuda_device=cuda:0 --gan_epochs=3 --a_epochs=10 --p_epochs=35
-python cli.py --relation_id=RTE --num_class=2 --batch_size=32 --lr=5e-5 --a_lr=5e-5 --p_lr=4e-3 --cuda_device=cuda:0 --gan_epochs=3 --a_epochs=10 --p_epochs=35
-python cli.py --relation_id=STSB --num_class=1 --batch_size=32 --lr=5e-5 --a_lr=5e-5 --p_lr=4e-3 --cuda_device=cuda:0 --gan_epochs=3 --a_epochs=10 --p_epochs=35
-python cli.py --relation_id=CoLA --num_class=2 --batch_size=32 --lr=5e-5 --a_lr=5e-5 --p_lr=4e-3 --cuda_device=cuda:0 --gan_epochs=3 --a_epochs=10 --p_epochs=35
-python cli.py --relation_id=RTE --num_class=2 --batch_size=32 --lr=5e-5 --a_lr=5e-5 --p_lr=4e-3 --cuda_device=cuda:0 --gan_epochs=3 --a_epochs=10 --p_epochs=35
-python cli.py --relation_id=STSB --num_class=1 --batch_size=32 --lr=5e-5 --a_lr=5e-5 --p_lr=4e-3 --cuda_device=cuda:0 --gan_epochs=3 --a_epochs=10 --p_epochs=35
-python cli.py --relation_id=CoLA --num_class=2 --batch_size=32 --lr=5e-5 --a_lr=5e-5 --p_lr=4e-3 --cuda_device=cuda:0 --gan_epochs=3 --a_epochs=10 --p_epochs=35
-python cli.py --relation_id=RTE --num_class=2 --batch_size=32 --lr=5e-5 --a_lr=5e-5 --p_lr=4e-3 --cuda_device=cuda:0 --gan_epochs=3 --a_epochs=10 --p_epochs=35
-python cli.py --relation_id=STSB --num_class=1 --batch_size=32 --lr=5e-5 --a_lr=5e-5 --p_lr=4e-3 --cuda_device=cuda:0 --gan_epochs=3 --a_epochs=10 --p_epochs=35
+python cli.py --relation_id=CoLA --num_class=2 --batch_size=32 --lr=5e-5 --a_lr=5e-5 --p_lr=4e-3 --cuda_device=cuda:0 --gan_epochs=2 --a_epochs=30 --p_epochs=30
+python cli.py --relation_id=RTE --num_class=2 --batch_size=32 --lr=5e-5 --a_lr=5e-5 --p_lr=4e-3 --cuda_device=cuda:0 --gan_epochs=2 --a_epochs=30 --p_epochs=30
+python cli.py --relation_id=STSB --num_class=1 --batch_size=32 --lr=5e-5 --a_lr=5e-5 --p_lr=4e-3 --cuda_device=cuda:0 --gan_epochs=2 --a_epochs=30 --p_epochs=30
